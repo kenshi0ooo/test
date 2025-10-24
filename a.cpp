00c1bf7e6ff9f092318dc8975afa7585e2eed1d1
kenshi0ooo <spartanetse@gmail.com>
2025-10-24 13:10:33 +0300

    ------

---
+++ a.cpp
@@ -0,0 +1,15 @@
+#include <cstdint>
+#include <iostream>
+
+int main()
+{
+
+std::int32_t a = 10;
+    std::int32_t b = 2;
+    std::int32_t sum = a + b;
+
+    std::cout << "The sum of " << a << " and " << b << " is " << sum << std::endl;
+
+    return 0;
+    
+}
